3941a3ea58e4fb5cc9f51d8007895dd6e40c7d7f
Klisman Evangelista <klismanevangelista777@gmail.com>
2024-08-30 17:17:56 -0500

    Ejercicio part1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,37 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update]
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'lists', to: 'lists#index'
-  get 'lists/new', to: 'lists#new'
-  post 'lists', to: 'lists#create'
-  get 'lists/:id', to: 'lists#show', as: :list
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
